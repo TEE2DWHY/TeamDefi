b652aad364706fca22cd4568974e8bffafa705ca
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-09-06 13:57:12 +0100

    made changes in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className="navbar  sticky-top navbar-expand-lg nav-header" >
             <div className="container-fluid">
                 <a className="navbar-brand" href="/#" style={{ color: " #fff" }}>Logo</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
