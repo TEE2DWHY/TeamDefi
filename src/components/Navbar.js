623198de47b36125c158c1e80cdeb9275fcfd8eb
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-09-06 13:38:53 +0100

    build updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className="navbar  sticky-top navbar-expand-lg nav-header" >
             <div className="container-fluid">
                 <a className="navbar-brand" href="/#" style={{ color: " #fff" }}>Logo</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -29,7 +29,7 @@ function Navbar() {
                             </ul>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" style={{ color: " #fff" }}>Disabled</a>
+                            <a className="nav-link" href="/#" style={{ color: " #fff" }}>Projects</a>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
