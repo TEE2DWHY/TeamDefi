487d08696a21e013a5dfd6a1261208b0c28e7488
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-10-08 13:32:42 +0100

    made changes on card again

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -7,7 +7,7 @@ function Learn(props) {
         <>
 
             <div className="col-lg-4">
-                <div className="card course-card" style={{ width: "19em" }}>
+                <div className="card course-card" style={{ width: "21em" }}>
                     <div className="card-body">
                         <h5 className="card-title"><i class="fa-solid fa-gears"></i> {props.courseTitle}</h5>
                         {/* <h6 className="card-subtitle mb-2 text-muted">{props.courseContent}</h6> */}
--- src/components/Features.js
+++ src/components/Features.js
@@ -6,7 +6,7 @@ function Features(props) {
         <>
 
             <div className="col-lg-4">
-                <div className="card course-card" style={{ width: "19em" }}>
+                <div className="card course-card" style={{ width: "21em" }}>
                     <div className="card-body">
                         <h5 className="card-title">{props.title}</h5>
                         <h6 className="card-subtitle mb-2 text-muted">{props.logo}</h6>
