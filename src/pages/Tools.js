874cf743d93a2243ed84cac4c5aaef55dfd538ce
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-10-12 09:43:13 +0100

    made some changes on the css button

--- src/pages/Tools.js
+++ src/pages/Tools.js
@@ -11,28 +11,27 @@ function Tools() {
             </div>
             <br />
             <h1 style={{ fontWeight: "900", textAlign: "center" }}>Tools</h1>
-            <br /> <br />
+            <br />
             <div className="container">
-                <h3 style={{ fontWeight: "900" }}>Blockchain Explorer</h3>
+                <h3 style={{ fontWeight: "900" }}>Blockchain explorer</h3>
                 <br />
                 <div className="container tool">
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="tools-cont tools-card">
-                                <div className="img-cont" style={{ backgroundColor: "#467df9", height: "80px", width: "100%", borderRadius: "7px", padding: "25px" }}>
+                                <div className="img-cont" style={{ backgroundColor: "#467df9", height: "80px", width: "100%", borderRadius: "14px 13px 0px 0px", padding: "25px" }}>
                                     <img className="blockchain-logo" src="Blockchain_logo.webp" alt="blockchain-logo" />
                                 </div>
                                 <div className="row" style={{ padding: "15px", marginTop: "5px" }}>
                                     <div className="col-lg-6">
-                                        <h3 style={{ fontWeight: "700" }}>Blockchain</h3>
+                                        <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Blockchain</h3>
                                     </div>
-                                    <div className="col-lg-6" style={{ textAlign: "right" }}>
-                                        <button className="btn btn-outline-success" type="button" style={{ padding: "6px" }}>visit site <i class="fa-solid fa-share"></i></button>
+                                    <div className="col-lg-6" style={{ textAlign: "center" }}>
+                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <br /> <br />
                                     </div>
+                                    <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>Blockchain.com claims to be the most secure exchange to buy, sell and trade cryptocurrency.</p>
                                 </div>
-                                <br />
-                                <p style={{ opacity: "0.6", padding: "6px" }}>Blockchain.com claims to be the most secure exchange to buy, sell and trade cryptocurrency.</p>
-                                <br />
                             </div>
                         </div>
                         <div className="col-lg-6">
