d8ec5f73c04d9120a59a7868f20f787c5129e0ad
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-10-12 12:08:42 +0100

    worked on some chnages

--- src/pages/Tools.js
+++ src/pages/Tools.js
@@ -27,7 +27,7 @@ function Tools() {
                                         <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Blockchain</h3>
                                     </div>
                                     <div className="col-lg-6" style={{ textAlign: "center" }}>
-                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <a href="https://www.blockchain.com/"><button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button></a>
                                         <br /> <br />
                                     </div>
                                     <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>Blockchain.com claims to be the most secure exchange to buy, sell and trade cryptocurrency. Trusted by millions since 2011 with over $1 Trillion in crypto transactions.</p>
@@ -44,7 +44,7 @@ function Tools() {
                                         <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Etherscan</h3>
                                     </div>
                                     <div className="col-lg-6" style={{ textAlign: "center" }}>
-                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <a href="https://etherscan.io/"><button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button></a>
                                         <br /> <br />
                                     </div>
                                     <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>Etherscan allows you to explore and search for transactions, addresses, tokens, prices and other activities taking place on the Ethereum blockchain.</p>
@@ -70,7 +70,7 @@ function Tools() {
                                         <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Coinigy</h3>
                                     </div>
                                     <div className="col-lg-6" style={{ textAlign: "center" }}>
-                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <a href="https://www.coinigy.com/"><button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button></a>
                                         <br /> <br />
                                     </div>
                                     <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>Coinigy lets you trade on every Bitcoin exchange through one account. Coinigy is a powerful and intuitive tool for managing your digital wallet portfolio.</p>
@@ -87,7 +87,7 @@ function Tools() {
                                         <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Tradedash</h3>
                                     </div>
                                     <div className="col-lg-6" style={{ textAlign: "center" }}>
-                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <a href="https://global.bittrex.com/trade/btc-usd"><button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button></a>
                                         <br /> <br />
                                     </div>
                                     <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>Tradedash is a trading tool that provides professional and personal customers with a secure, customizable trading experience.Tradedash offers real-time data from support trading platforms. </p>
@@ -113,7 +113,7 @@ function Tools() {
                                         <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>TradingView</h3>
                                     </div>
                                     <div className="col-lg-6" style={{ textAlign: "center" }}>
-                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <a href="https://www.tradingview.com/"> <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button></a>
                                         <br /> <br />
                                     </div>
                                     <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>TradingView is a social network for traders and investors with live quotes, stock charts and expert trading ideas.</p>
@@ -130,10 +130,97 @@ function Tools() {
                                         <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Cryptowat.ch</h3>
                                     </div>
                                     <div className="col-lg-6" style={{ textAlign: "center" }}>
-                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <a href="https://cryptowat.ch/"><button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button></a>
                                         <br /> <br />
                                     </div>
                                     <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>Watch, analyze and trade Bitcoin, Ethereum, and more on every exchange from one terminal.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <br /> <br />
+            <div className="container">
+                <h2 style={{ fontWeight: "900", letterSpacing: "-.03em", lineHeight: "1.2" }}>Market data</h2>
+                <br />
+                <div className="container tool">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="tools-cont tools-card">
+                                <div className="img-cont" style={{ backgroundColor: "#467df9", height: "80px", width: "100%", borderRadius: "14px 13px 0px 0px", padding: "25px" }}>
+                                    <img className="coinmarketcap-logo" src="Coinmarketcap_logo.webp" alt="coinmarketcap-logo" />
+                                </div>
+                                <div className="row" style={{ padding: "15px", marginTop: "5px" }}>
+                                    <div className="col-lg-6">
+                                        <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Coinmarketcap</h3>
+                                    </div>
+                                    <div className="col-lg-6" style={{ textAlign: "center" }}>
+                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <br /> <br />
+                                    </div>
+                                    <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>Cryptocurrency market cap rankings, charts, and more.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="tools-cont tools-card">
+                                <div className="img-cont" style={{ backgroundColor: "#467df9", height: "80px", width: "100%", borderRadius: "14px 13px 0px 0px", padding: "25px" }}>
+                                    <img className="cryptocompare-logo" src="CryptoCompare_logo.webp" alt="cryptocompare-logo" />
+                                </div>
+                                <div className="row" style={{ padding: "15px", marginTop: "5px" }}>
+                                    <div className="col-lg-6">
+                                        <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>CryptoCompare</h3>
+                                    </div>
+                                    <div className="col-lg-6" style={{ textAlign: "center" }}>
+                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <br /> <br />
+                                    </div>
+                                    <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>All the latest streaming pricing data in the world of cryptocurrencies.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br /> <br />
+            <div className="container">
+                <h2 style={{ fontWeight: "900", letterSpacing: "-.03em", lineHeight: "1.2" }}>Network data</h2>
+                <br />
+                <div className="container tool">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="tools-cont tools-card">
+                                <div className="img-cont" style={{ backgroundColor: "#467df9", height: "80px", width: "100%", borderRadius: "14px 13px 0px 0px", padding: "25px" }}>
+                                    <img className="Graphana-logo" src="Graphana Optech Dashboards_logo.webp" alt="Graphana-logo" />
+                                </div>
+                                <div className="row" style={{ padding: "15px", marginTop: "5px" }}>
+                                    <div className="col-lg-6">
+                                        <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Graphana Optech Dashboards</h3>
+                                    </div>
+                                    <div className="col-lg-6" style={{ textAlign: "center" }}>
+                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <br /> <br />
+                                    </div>
+                                    <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}>Grafana.com provides a central repository where the community can come together to discover and share dashboards.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="tools-cont tools-card">
+                                <div className="img-cont" style={{ backgroundColor: "#467df9", height: "80px", width: "100%", borderRadius: "14px 13px 0px 0px", padding: "25px" }}>
+                                    <img className="txstats-logo" src="Txstats_logo.webp" alt="txstats-logo" />
+                                </div>
+                                <div className="row" style={{ padding: "15px", marginTop: "5px" }}>
+                                    <div className="col-lg-6">
+                                        <h3 style={{ fontWeight: "700", marginBottom: "15px" }}>Txstats</h3>
+                                    </div>
+                                    <div className="col-lg-6" style={{ textAlign: "center" }}>
+                                        <button className="visit-site"> visit site <i class="fa-solid fa-arrow-right"></i></button>
+                                        <br /> <br />
+                                    </div>
+                                    <p style={{ opacity: "0.6", padding: "6px", fontSize: "14px", margin: "10px" }}> <br />Txstats.com provides a series of dashboards centered around a specific element of Bitcoin transactions.</p>
                                 </div>
                             </div>
                         </div>
