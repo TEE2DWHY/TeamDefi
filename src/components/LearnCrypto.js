0f94d5bfc90729e05a381373730efbbb54ebf7bf
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-10-19 18:32:15 +0100

    made changes on kickstart button

--- src/components/LearnCrypto.js
+++ src/components/LearnCrypto.js
@@ -8,13 +8,13 @@ function LearnCrypto() {
                     <div className="row">
                         <div className="col-lg-6">
                             <h1 style={{ color: "#000" }} className="learncrypto">About Team Defi...</h1>
-                            <p style={{ color: "#000", marginTop: "26px", fontWeight: "bold", opacity: "0.6" }}>
+                            <p style={{ marginTop: "26px", opacity: "0.6" }}>
 
                                 Team Defi is a an educational platform designed to help users easily learn about cryptocurrency, with simple, relevant and engaging content.
                                 <br /> <br />
                                 You can proceed by starting with the basics of cryptocurrecies or choose from our other available courses.
                             </p>
-                            <Link to="/knowledge-base"><button className="btn btn-success cta" type="button">Kickstart <i style={{ color: "#fff" }} class="fa-brands fa-kickstarter-k"></i></button></Link>
+                            <Link to="/knowledge-base"><button className="btn btn-success kickstart" type="button">Kickstart <i style={{ color: "#fff" }} class="fa-brands fa-kickstarter-k"></i></button></Link>
                         </div>
                         <div className="col-lg-6">
                             <img className="about" src="about.png" alt="" />
