19a20737ecec9b2807844b856449c8fad7d17f4c
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-11-20 14:39:43 +0100

    made changes on the blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -65,7 +65,7 @@ const Blog = (props) => {
                         <li className="page-item"><a class="page-link" href="/#">2</a></li>
                         <li className="page-item"><a class="page-link" href="/#">3</a></li>
                         <li className="page-item">
-                            <a className="page-link" href="#" aria-label="Next">
+                            <a className="page-link" href="/#" aria-label="Next">
                                 <span aria-hidden="true">&raquo;</span>
                             </a>
                         </li>
