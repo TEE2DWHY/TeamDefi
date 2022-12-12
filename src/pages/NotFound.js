f8d8a5ddbbfde48a0c7f828c12af5593f18c1fff
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-12-12 09:09:07 +0100

    worked on the not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
             <div className="container" style={{ textAlign: "center" }}>
                 <img className="img-fluid" src="404.gif" alt="" />
                 <h3>Oh no, something went wrong!</h3>
-                <p style={{ opacity: "0.6" }}>Either something went wrong or this page doesn't exist anymore.</p>
+                <p style={{ opacity: "0.6" }}>This route doesn't exist.😐</p>
                 <Link to="/"><button className="btn btn-primary" style={{ padding: "10px 20px" }}>Take me to HomePage</button></Link>
                 <br /> <br />
             </div>
