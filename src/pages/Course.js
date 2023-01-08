d7400caf7d2931d7fd42c9bb15e51f6f0cea2719
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-01-08 18:52:11 +0100

    started working on the courses page

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -36,7 +36,7 @@ const Courses = () => {
                         </div>
                         <div className='col-lg-6'>
                             <h2 style={{ color: "#fff", marginTop: "10%" }}>The Complete <br />Crypto Stack Course: <br /> </h2>
-                            <p style={{ color: "#fff", fontSize: "16px", marginTop: "5%" }}>Your full guide into the world of cryptocurrency. In this course we cover every of the technicalities ranging from bitcoins origination, decentralized financing, fundamentals and lots more.</p>
+                            <p style={{ color: "#fff", fontSize: "16px", marginTop: "5%", padding: "10px" }}>Your full guide into the world of cryptocurrency. In this course we cover every of the technicalities ranging from bitcoins origination, decentralized financing, fundamentals and lots more.</p>
                             <div className='academy-details'> <i class="fa-solid fa-person-chalkboard" style={{ color: "#fff" }}></i> 10 lessons</div>
                             <div className='academy-details'> <i class="fa-solid fa-clock" style={{ color: "#fff" }}></i> 8hrs:30Min</div>
                             <div className='academy-details'> <i class="fa-solid fa-chart-simple" style={{ color: "#fff" }}></i> Basics</div>
