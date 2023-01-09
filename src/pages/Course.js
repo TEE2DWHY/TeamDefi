63113b03f0f184b76c4bc49a5b719738609a0694
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-01-09 12:35:56 +0100

    started working on the courses page

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -62,6 +62,40 @@ const Courses = () => {
                         </div>
                     </div>
                 </div>
+                <br />
+                <div className='course-card2' style={{ cursor: "pointer" }}>
+                    <div className='row'>
+                        <div className='col-lg-6'>
+                            <img className='academy1' src="academy1.webp" alt='academy' />
+                        </div>
+                        <div className='col-lg-6'>
+                            <h3 style={{ color: "#fff", marginTop: "5%", fontSize: "20px", }}>The Complete <br />Crypto Stack Course: <br /> </h3>
+                            <p style={{ color: "#fff", fontSize: "14px", padding: "0 5px 0 0" }}>In this course we cover a variety of topics from <br />bitcoin as an asset, de-fi, fundamentals and profiting with crypto etc.</p>
+                            <div className='academy-details'> <i class="fa-solid fa-person-chalkboard" style={{ color: "#fff" }}></i> 10 lessons</div>
+                            <div className='academy-details'> <i class="fa-solid fa-clock" style={{ color: "#fff" }}></i> 8hrs:30Min</div>
+                            <div className='academy-details'> <i class="fa-solid fa-chart-simple" style={{ color: "#fff" }}></i> Basics</div>
+
+                            <Link to="/course"><button className='academy-button'>Start Learning</button></Link>
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div className='course-card2' style={{ cursor: "pointer" }}>
+                    <div className='row'>
+                        <div className='col-lg-6'>
+                            <img className='academy1' src="academy1.webp" alt='academy' />
+                        </div>
+                        <div className='col-lg-6'>
+                            <h3 style={{ color: "#fff", marginTop: "5%", fontSize: "20px", }}>The Complete <br />Crypto Stack Course: <br /> </h3>
+                            <p style={{ color: "#fff", fontSize: "14px", padding: "0 5px 0 0" }}>In this course we cover a variety of topics from <br />bitcoin as an asset, de-fi, fundamentals and profiting with crypto etc.</p>
+                            <div className='academy-details'> <i class="fa-solid fa-person-chalkboard" style={{ color: "#fff" }}></i> 10 lessons</div>
+                            <div className='academy-details'> <i class="fa-solid fa-clock" style={{ color: "#fff" }}></i> 8hrs:30Min</div>
+                            <div className='academy-details'> <i class="fa-solid fa-chart-simple" style={{ color: "#fff" }}></i> Basics</div>
+
+                            <Link to="/course"><button className='academy-button'>Start Learning</button></Link>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </>
