16e1bb553d96ac4f39886e5267faf20ecf37be82
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-01-10 23:57:05 +0100

    started working on the privacy page

--- src/pages/Privacy.js
+++ src/pages/Privacy.js
@@ -23,7 +23,11 @@ const Privacy = () => {
                 <p style={{ opacity: "0.6" }}>Our main goal in gathering personal data about you is to give you a safe, comfortable, effective, and customized experience. Your personal information won't be kept by us any longer than is required by law or for business needs. We utilize the data we gather from our services to deliver, maintain, safeguard, and improve them as well as to create new ones, safeguard our website, and safeguard the privacy of our users.</p>
                 <br /> <br />
                 <p style={{ fontWeight: "bold" }}>Deleting the account</p>
-                <p style={{ opacity: "0.6" }}></p>
+                <p style={{ opacity: "0.6" }}>Your personal information is kept on file by Team De-Fi for as long as is necessary to grant you access to the website's services. You might be able to deactivate your user account if you no longer want to use our application by emailing a request to <a href="mailto:decentralizedfinancing101@gmail.com">decentralizedfinancing101@gmail.com</a>. If you want to permanently remove your personal information from our servers, let us know in the email.</p>
+                <br /> <br />
+                <p style={{ fontWeight: "bold" }}>Changes</p>
+                <p style={{ opacity: "0.6" }}>There will be periodic updates to both the Terms & Conditions and this Privacy Policy. Any updates or modifications will be posted on the website. To make sure you are familiar with the most recent version of the Terms, we urge you to check this page from time to time.
+                    You can contact <a href="mailto:decentralizedfinancing101@gmail.com">decentralizedfinancing101@gmail.com</a> with any requests, questions, or concerns regarding the processing of your personal data.</p>
             </div>
             <Footer />
         </>
