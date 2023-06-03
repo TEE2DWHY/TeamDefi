db2301aee270d057ec2b730b6c33d9c2d8090ea2
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-06-03 16:28:12 +0100

    changed the webpage font

--- src/components/Hero-Section.js
+++ src/components/Hero-Section.js
@@ -2,26 +2,46 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function HeroSection() {
-    // const user = false
-    return (
-        <>
-            <div className="Hero-Section">
-                <br /> <br />
-                <div className="welcome-message">
-                    <h1 className="cta">Welcome to
-                        <br /> Our Aca<span style={{ color: "#1bd190" }}>demy</span></h1>
-                    <br />
-                    <p style={{ color: "#fff", padding: "0 25px" }}>At Team DeFi we introduce our users to the world of cryptocurrency.<br />
-                        One of the major obstacles to understanding crypto is that it is both complex and unfamiliar.
-                        <br /><span style={{ fontStyle: "italic" }}>Have no fears. We're here to help! <i className="fa-solid fa-handshake-angle" style={{ color: "#fff" }}></i></span></p>
-                    <img className="learn-img" src="learn.png" alt="" />
-                    <Link to="/academy"><button className="btn btn-success subscribe" type="submit">Proceed to Academy <i class="fa-solid fa-graduation-cap" style={{ color: "#fff" }}></i></button></Link>
-                    <br /> <br />
-                </div>
-            </div>
-
-        </>
-    )
+  // const user = false
+  return (
+    <>
+      <div className="Hero-Section">
+        <br /> <br />
+        <div className="welcome-message">
+          <h1 className="cta">
+            Welcome to
+            <br /> Our Aca<span style={{ color: "#1bd190" }}>demy</span>
+          </h1>
+          <br />
+          <p style={{ color: "#fff", padding: "0 20px" }}>
+            At Team DeFi we introduce our users to the world of cryptocurrency.
+            <br />
+            One of the major obstacles to understanding crypto is that it is
+            both complex and unfamiliar.
+            <br />
+            <span style={{ fontStyle: "italic" }}>
+              Have no fears. We're here to help!{" "}
+              <i
+                className="fa-solid fa-handshake-angle"
+                style={{ color: "#fff" }}
+              ></i>
+            </span>
+          </p>
+          <img className="learn-img" src="learn.png" alt="" />
+          <Link to="/academy">
+            <button className="btn btn-success subscribe" type="submit">
+              Proceed to Academy{" "}
+              <i
+                class="fa-solid fa-graduation-cap"
+                style={{ color: "#fff" }}
+              ></i>
+            </button>
+          </Link>
+          <br /> <br />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,192 +1,271 @@
-import Footer from "../components/Footer"
-import { Link } from "react-router-dom"
-import axios from "axios"
-import { useState } from "react"
-import "../assets/auth.css"
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import "../assets/auth.css";
 // require('dotenv').config()
 // import "../.env"
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+    terms: false,
+  });
 
-    const [formData, setFormData] = useState({
-        fullName: "", email: "", phoneNumber: "", password: "", confirmPassword: "", terms: false
-    })
+  const handleChange = (e) => {
+    const { name, type, checked, value } = e.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
 
-    const handleChange = (e) => {
-        const { name, type, checked, value } = e.target
-        setFormData((prevFormData) => {
-            return {
-                ...prevFormData,
-                [name]: type === "checkbox" ? checked : value
-            }
-        })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/v1/auth/register",
+        formData
+      );
+      console.log(res);
+      // if (res.status === 200) {
+      //     window.location = "/sign-in"
+      // }
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(formData)
-        try {
-            const res = await axios.post("http://localhost:5000/api/v1/auth/register", formData)
-            console.log(res)
-            // if (res.status === 200) {
-            //     window.location = "/sign-in"
-            // }
-        } catch (err) {
-            console.log(err)
-        }
+  const handlePassword = () => {
+    var showPassword = document.getElementById("password");
+    if (showPassword.type === "text") {
+      showPassword.type = "password";
+      document.getElementById("hide-password").style.display = "inline";
+      document.getElementById("password-reveal").style.display = "none";
+    } else {
+      showPassword.type = "text";
+      document.getElementById("password-reveal").style.display = "inline";
+      document.getElementById("hide-password").style.display = "none";
     }
-
-    const handlePassword = () => {
-        var showPassword = document.getElementById("password")
-        if (showPassword.type === "text") {
-            showPassword.type = "password"
-            document.getElementById("hide-password").style.display = "inline"
-            document.getElementById("password-reveal").style.display = "none"
-        }
-        else {
-            showPassword.type = "text"
-            document.getElementById("password-reveal").style.display = "inline"
-            document.getElementById("hide-password").style.display = "none"
-        }
-
+  };
+  const handleConfirmPassword = () => {
+    var showPassword = document.getElementById("confirm-password");
+    if (showPassword.type === "text") {
+      showPassword.type = "password";
+      document.getElementById("hide-confirmPassword").style.display = "inline";
+      document.getElementById("confirmPassword-reveal").style.display = "none";
+    } else {
+      showPassword.type = "text";
+      document.getElementById("confirmPassword-reveal").style.display =
+        "inline";
+      document.getElementById("hide-confirmPassword").style.display = "none";
     }
-    const handleConfirmPassword = () => {
-        var showPassword = document.getElementById("confirm-password")
-        if (showPassword.type === "text") {
-            showPassword.type = "password"
-            document.getElementById("hide-confirmPassword").style.display = "inline"
-            document.getElementById("confirmPassword-reveal").style.display = "none"
-        }
-        else {
-            showPassword.type = "text"
-            document.getElementById("confirmPassword-reveal").style.display = "inline"
-            document.getElementById("hide-confirmPassword").style.display = "none"
-        }
+  };
 
-    }
-
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg nav">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/academy" style={{ color: " #fff" }}><img className="logo" src="blockchain.png" alt="" /></Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/sign-in"><button className="sign-in">Sign in</button></a>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/academy"><i class="fa-solid fa-house"></i> Campus</Link>
-                            </li>
-                            {/* <li className="nav-item">
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg nav">
+        <div className="container-fluid">
+          <Link
+            className="navbar-brand"
+            to="/academy"
+            style={{ color: " #fff" }}
+          >
+            <img className="logo" src="blockchain.png" alt="" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/sign-in">
+                  <button className="sign-in">Sign in</button>
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/academy"
+                >
+                  <i class="fa-solid fa-house"></i> Campus
+                </Link>
+              </li>
+              {/* <li className="nav-item">
                                 <a className="nav-link" href="#library"><i className="fa-solid fa-book-open-reader"></i> Library</a>
                             </li> */}
-                        </ul>
-                    </div>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <br />
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-lg-6 col-md-6 col-sm-6">
+              <img
+                className="img-fluid"
+                src="Privacy policy.gif"
+                alt="login"
+                style={{ display: "block", margin: "auto" }}
+              />
+              {/* <p>Lets plug you into the world of cryptocurrencies.</p> */}
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-6 login-form">
+              <form onSubmit={handleSubmit}>
+                <div className="form-component">
+                  <input
+                    className="login-input"
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                    required
+                    onChange={handleChange}
+                    pattern="^[A-Za-z\s]*$"
+                  />
+                  <span className="user-err">
+                    First name should be only alphabets
+                  </span>
                 </div>
-            </nav>
-            <br />
-            <section>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6 col-lg-6 col-md-6 col-sm-6">
-                            <img className="img-fluid" src="Privacy policy.gif" alt="login" style={{ display: "block", margin: "auto" }} />
-                            {/* <p>Lets plug you into the world of cryptocurrencies.</p> */}
-                        </div>
-                        <div className="col-lg-6 col-md-6 col-sm-6 login-form">
-                            <form onSubmit={handleSubmit}>
-                                <div className="form-component">
-                                    <input
-                                        className="login-input"
-                                        type="text"
-                                        placeholder="Full Name"
-                                        name="fullName"
-                                        required
-                                        onChange={handleChange}
-                                        pattern="^[A-Za-z\s]*$"
-                                    />
-                                    <span className="user-err">First name should be only alphabets</span>
-                                </div>
-                                <div className="form-component">
-                                    <input
-                                        className="login-input"
-                                        type="email"
-                                        placeholder="Email address"
-                                        name="email"
-                                        required
-                                        onChange={handleChange}
-                                    />
-                                    <span className="user-err">Enter a valid email address e.g satoshi@gmail.com</span>
-                                </div>
-                                <div className="form-component">
-                                    <input
-                                        className="login-input"
-                                        type="text"
-                                        placeholder="Phone Number"
-                                        name="phoneNumber"
-                                        required
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                                <div className="form-component">
-                                    <input
-                                        className="login-input"
-                                        type="password"
-                                        placeholder="Password"
-                                        name="password"
-                                        required
-                                        onChange={handleChange}
-                                        pattern=".{8,}"
-                                        id="password"
-                                    />
-                                    <i className="fa-solid fa-eye password-icons" onClick={handlePassword} id="password-reveal"></i>
-                                    <i className="fa-solid fa-eye-slash password-icons" id="hide-password" onClick={handlePassword}></i>
-                                    <span className="user-err">password should be minimum of 8 characters</span>
-                                </div>
-                                <div className="form-component">
-                                    <input
-                                        className="login-input"
-                                        type="password"
-                                        placeholder="Confirm Password"
-                                        name="confirmPassword"
-                                        required
-                                        onChange={handleChange}
-                                        pattern={formData.password}
-                                        onFocus="true"
-                                        id="confirm-password"
-                                    />
-                                    <i className="fa-solid fa-eye password-icons" onClick={handleConfirmPassword} id="confirmPassword-reveal"></i>
-                                    <i className="fa-solid fa-eye-slash password-icons" id="hide-confirmPassword" onClick={handleConfirmPassword}></i>
-                                    <span className="user-err">password do not match</span>
-                                </div>
-                                <div className="form-component">
-                                    <input
-                                        type="checkbox"
-                                        required
-                                        name="terms"
-                                        onChange={handleChange}
-                                        checked={formData.terms}
-                                    /> <label style={{ fontSize: "14px" }}>
-                                        Agree to <Link to="/privacy" style={{ textDecoration: "underline", color: "#5181f1" }}>terms and conditions</Link>
-                                    </label>
-                                </div>
-                                <button className="btn-login">Create Account</button>
-                                <br /> <br />
-                            </form>
-                            <hr />
-                            <span style={{ color: "#000", opacity: "0.6" }}>Already have an account? <i class="fa-solid fa-arrow-down"></i></span>
-                            <br /> <br />
-                            <Link to="/sign-in"><button className="create-account">Login</button></Link>
-                        </div>
-                    </div>
+                <div>
+                  <input
+                    className="login-input"
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                    required
+                    onChange={handleChange}
+                    pattern="^[A-Za-z\s]*$"
+                  />
+                  <span className="user-err">
+                    Last name should be only alphabets
+                  </span>
                 </div>
-            </section>
-            <Footer />
-        </>
-    )
-}
+                <div className="form-component">
+                  <input
+                    className="login-input"
+                    type="email"
+                    placeholder="Email address"
+                    name="email"
+                    required
+                    onChange={handleChange}
+                  />
+                  <span className="user-err">
+                    Enter a valid email address e.g satoshi@gmail.com
+                  </span>
+                </div>
+                <div className="form-component">
+                  <input
+                    className="login-input"
+                    type="text"
+                    placeholder="Phone Number"
+                    name="phoneNumber"
+                    required
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-component">
+                  <input
+                    className="login-input"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    required
+                    onChange={handleChange}
+                    pattern=".{8,}"
+                    id="password"
+                  />
+                  <i
+                    className="fa-solid fa-eye password-icons"
+                    onClick={handlePassword}
+                    id="password-reveal"
+                  ></i>
+                  <i
+                    className="fa-solid fa-eye-slash password-icons"
+                    id="hide-password"
+                    onClick={handlePassword}
+                  ></i>
+                  <span className="user-err">
+                    password should be minimum of 8 characters
+                  </span>
+                </div>
+                <div className="form-component">
+                  <input
+                    className="login-input"
+                    type="password"
+                    placeholder="Confirm Password"
+                    name="confirmPassword"
+                    required
+                    onChange={handleChange}
+                    pattern={formData.password}
+                    onFocus="true"
+                    id="confirm-password"
+                  />
+                  <i
+                    className="fa-solid fa-eye password-icons"
+                    onClick={handleConfirmPassword}
+                    id="confirmPassword-reveal"
+                  ></i>
+                  <i
+                    className="fa-solid fa-eye-slash password-icons"
+                    id="hide-confirmPassword"
+                    onClick={handleConfirmPassword}
+                  ></i>
+                  <span className="user-err">password do not match</span>
+                </div>
+                <div className="form-component">
+                  <input
+                    type="checkbox"
+                    required
+                    name="terms"
+                    onChange={handleChange}
+                    checked={formData.terms}
+                  />{" "}
+                  <label style={{ fontSize: "14px" }}>
+                    Agree to{" "}
+                    <Link
+                      to="/privacy"
+                      style={{ textDecoration: "underline", color: "#5181f1" }}
+                    >
+                      terms and conditions
+                    </Link>
+                  </label>
+                </div>
+                <button className="btn-login">Create Account</button>
+                <br /> <br />
+              </form>
+              <hr />
+              <span style={{ color: "#000", opacity: "0.6" }}>
+                Already have an account? <i class="fa-solid fa-arrow-down"></i>
+              </span>
+              <br /> <br />
+              <Link to="/sign-in">
+                <button className="create-account">Login</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
 
-export default SignUp
+export default SignUp;
